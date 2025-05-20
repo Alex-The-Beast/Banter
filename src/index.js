1cf1b0f8d3be6a0e7e155771935a58a3ffb87025
Gaurav kumar <gk064577@gmail.com>
2025-05-20 20:56:58 +0530

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import cors from 'cors'
 import express from 'express';
 import {createServer} from 'http'
 import { StatusCodes } from 'http-status-codes';
@@ -14,7 +14,7 @@ import apiRouter from './routes/apiRoutes.js'
 
 
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));  
 
